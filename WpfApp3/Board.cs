cea6c6fd8c9b0bdebbb1903b219705903cdab0b1
Tony Collins <tjc@eltham-college.org.uk>
2024-06-11 15:56:32 +0100

    Add project files.

---
+++ WpfApp3/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class Board
+    {
+        private int colour = 5;
+        public Board()
+        {
+
+        }
+
+        public void ChangeSomething()
+        {
+            colour = colour + 1;
+        }
+        public int col
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+    }
+}
---
+++ WpfApp3/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class Game
+    {
+        private Board b;
+        public Game()
+        {
+            b = new Board();
+        }
+
+        public void ChangeSomething()
+        {
+            b.ChangeSomething();
+        }
+        public Board board {
+            get { return b; }
+            set { b = value; }
+        }
+        
+    
+    }
+}
